f60e0b2849e8b64fb87e6a56fcb1c42bd1f34442
damiiegregorio <damiiekeith@gmail.com>
2019-10-29 14:52:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import csv
 
 
 def login(username, word):
+    """Login function"""
     pwd = getpass.getpass()
     r = requests.get('https://api.github.com/users', auth=(username, pwd))
     if r.status_code == 200:
         search(word)
-        # csv_file(word)
     else:
         print('Authentication failed.')
 
 
 def search(word):
+    """Search repositories"""
     list_comprehension = [i for i in range(1, 10)]
 
     for page in list_comprehension:
@@ -38,12 +39,14 @@ def search(word):
 
 
 def date_formatter():
+    """Format date today"""
     today = datetime.now().strftime("-%Y-%m-%d-%H-%M")
     date_today = "output" + today
     return date_today
 
 
 def csv_file(result):
+    """Creating CSV file"""
     date_today = date_formatter()
 
     with open(date_today + ".csv", 'a', encoding='utf-8', newline='') as output_file:
@@ -53,6 +56,7 @@ def csv_file(result):
 
 def main():
     try:
+        """Argument Parser for CLI"""
         parser = argparse.ArgumentParser()
         parser.add_argument('-u', '--username', type=str, help='Enter username')
         parser.add_argument('word', type=str, help='Enter keyword')
